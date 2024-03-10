8b26c9c9a14d4b86360cbe1269f4646918c0645f
ygursu <yasemingrs@gmail.com>
2024-03-10 23:32:44 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp132/Program.cs
@@ -0,0 +1,14 @@
+﻿double toplam = 0, sayac = 0, ortalama = 0;
+int i = 1;
+while (i <= 100)
+{
+    if (i % 2 == 0)
+    {
+        toplam = toplam + i;
+        sayac++;
+    }
+    i++;
+}
+ortalama = toplam / sayac;
+Console.WriteLine("Çift sayıların toplamı={0}\nAdet={1} Ortalaması={2}", toplam, sayac, ortalama);
+Console.ReadKey();
